eb4f978acdbbb4f3c8a4e3b2bb4974fff8bc39d0
Jason Feng <23126557+dyckia@users.noreply.github.com>
2020-07-13 11:34:00 -0230

    Update app.js

--- app.js
+++ app.js
@@ -32,16 +32,19 @@ function initMap() {
         position: { lat: parseFloat(airport[1]), lng: parseFloat(airport[2]) },
         label: airport[0]
     }));
+    
+    var markerCluster = new MarkerClusterer(map, markers, 
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
-    JFK = { lat: 40.63980103, lng: -73.77890015 }
-    LAX = { lat: 33.94250107, lng: -118.4079971 } 
-    SEA = { lat: 47.44900131, lng: -122.3089981 } 
-    MIA = { lat: 25.79319954, lng: -80.29060364 } 
-    MSP = { lat: 44.88199997, lng: -93.22180176 }
-    LAS = { lat: 36.08010101, lng: -115.1520004 } 
-    DWT = { lat: 42.21239853, lng: -83.35340118 }
-    IAH = { lat: 29.9843998, lng: -95.34140015 }
-    PDX = { lat: 45.58869934, lng: -122.5979996 }
+    JFK = { lat: 40.63980103, lng: -73.77890015 };
+    LAX = { lat: 33.94250107, lng: -118.4079971 };
+    SEA = { lat: 47.44900131, lng: -122.3089981 };
+    MIA = { lat: 25.79319954, lng: -80.29060364 };
+    MSP = { lat: 44.88199997, lng: -93.22180176 };
+    LAS = { lat: 36.08010101, lng: -115.1520004 };
+    DWT = { lat: 42.21239853, lng: -83.35340118 };
+    IAH = { lat: 29.9843998, lng: -95.34140015 };
+    PDX = { lat: 45.58869934, lng: -122.5979996 };
 
     var paths = [
         [[JFK, LAX], 14, '#fc0303'],
@@ -62,7 +65,4 @@ function initMap() {
 
         line.setMap(map);
     }
-
-    var markerCluster = new MarkerClusterer(map, markers, 
-        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
